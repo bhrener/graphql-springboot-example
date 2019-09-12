ea40a3f358f6792fef7f35b7bb67ef0a6adc848f
erick <erick.bhb@gmail.com>
2019-09-11 21:40:47 -0300

    First commit

---
+++ graphql-springboot-example/src/main/java/br/com/alfa/graphqlspringbootexample/GraphqlSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.alfa.graphqlspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootExampleApplication.class, args);
+	}
+
+}
---
+++ graphql-springboot-example/src/test/java/br/com/alfa/graphqlspringbootexample/GraphqlSpringbootExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.alfa.graphqlspringbootexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqlSpringbootExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
